c43d5d0fd674d44bb70bae9b30ecdc3fb8741157
Jinse <jinse@calibrate.be>
2022-06-08 17:33:54 +0200

    automatically select first tab on load in edit screen

--- resources/js/components/FormTabs.vue
+++ resources/js/components/FormTabs.vue
@@ -114,7 +114,7 @@ export default {
                 slug: this.$route.query.tab,
             });
         } else {
-            this.handleTabClick(tabs[Object.keys(tabs)[0]], false);
+            this.handleTabClick(tabs[Object.keys(tabs)[0]], true);
         }
     },
     methods: {
